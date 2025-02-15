283f3760e805401d04374e73f7c04c15ce6e6159
Will Keeble <will.a.keeble@gmail.com>
2025-02-15 12:58:25 +1100

    Initial project setup: Next.js frontend and Spring Boot backend

---
+++ backend/src/main/java/com/zrun/zrun_backend/ZrunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zrun.zrun_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZrunBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZrunBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zrun/zrun_backend/ZrunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrun.zrun_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZrunBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
